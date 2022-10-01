f9aa313667dee213769d77cba1e2680022f3f539
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-10-01 23:07:16 +0300

    first commit

---
+++ src/Content.js
@@ -0,0 +1,9 @@
+const Content = () => {
+    return (
+      <div>
+        <Part part1= {props.part1} exercises1 = {props.exercises1} />
+        <Part part2 = {props.part2} exercises2 = {props.exercises2}/>
+        <Part part3= {props.part3} exercises3 = {props.exercises3}/>
+      </div>
+    )
+  }
